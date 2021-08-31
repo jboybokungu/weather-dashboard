61e704be0b1b6d04989a932048136d2e27f90cd7
Jonathan Bokungu <jonathanbokungu21@gmail.com>
2021-08-30 23:18:32 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var myApiKey = "7944dc369c09bd11790514b47ecd9f9d";
+var clickButton = document.getElementById("select-button");
+
+
+
+//var searchCity = function (city) {
+//var cityApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myApiKey;
+
+//fetch(cityApi)
+  //  .then(function(response) {
+  //      return response.json();
+  //  })
+  //  .then(function(data) {
+  //      console.log("data",data);
+  //  })
+//}
+
+//clickButton.addEventListener('click', Event);
